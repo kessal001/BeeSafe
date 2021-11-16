74bab81f2be6887d3a40a1d484c80640e137e2cf
gracida001 <luca.pedersoli@hotmail.it>
2021-11-16 21:56:53 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+//ROUTES
+app.get('/',(req,res)=>{
+    res.send("We are on home!");
+})
+
+//Start listening to the server
+app.listen(3000);
