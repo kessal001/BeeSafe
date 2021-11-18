4f747ba2fe5d74440625f352545891ac692b8708
Luca Pedersoli <38606112+gracida001@users.noreply.github.com>
2021-11-18 21:02:35 +0100

    test

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
+require("dotenv/config");
+
+//Import routes
+
+const postRoute = require("./routes/posts");
+
+app.use("/posts",postRoute);
 
 //ROUTES
 app.get('/',(req,res)=>{
     res.send("We are on home!");
 })
 
+//Connect To DB
+mongoose.connect(process.env.DB_CONNECTION,()=>{
+    console.log("Connected to the DB");
+})
 //Start listening to the server
-app.listen(2001);
+app.listen(3000);
